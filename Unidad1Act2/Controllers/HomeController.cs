41de684f358b526d27caa25221a0c3b1784b9468
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2022-09-18 16:38:57 -0500

    Agregar archivos de proyecto.

---
+++ Unidad1Act2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unidad1Act2.Models;
+
+namespace Unidad1Act2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(Calificacion calificacion)
+        {
+           
+
+            if (calificacion.Materia1 >= 0 && calificacion.Materia1 <= 100 &&
+                calificacion.Materia2 >= 0 && calificacion.Materia2 <= 100 &&
+                calificacion.Materia3 >= 0 && calificacion.Materia3 <= 100)
+
+                calificacion.Promedio = (calificacion.Materia1 + calificacion.Materia2 + calificacion.Materia3) / 3;
+            else
+                calificacion.Materia1 = calificacion.Materia2 = calificacion.Materia3 = 0;
+
+            return View(calificacion);
+        }
+    }
+}
---
+++ Unidad1Act2/Models/Calificacion.cs
@@ -0,0 +1,14 @@
+﻿namespace Unidad1Act2.Models
+{
+    public class Calificacion
+    {
+        public double Materia1 { get; set; }
+
+        public double Materia2 { get; set; }
+
+        public double Materia3 { get; set; }
+
+        public double Promedio { get; set; }
+
+    }
+}
